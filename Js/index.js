14477d01331d5387f0f5a07a24f5147405c8f24b
Shaurya Vardhan <shaurya.vardhan.9758@gmail.com>
2022-02-04 01:28:28 +0530

    blog

---
+++ Js/index.js
@@ -0,0 +1,35 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance:'80px',
+    duration: 2000,
+    reset: true
+});
+// sr.reveal('.type',{});
+sr.reveal('.hey',{delay:100});
+sr.reveal('.para-content-1',{delay:100
+,origin:'left'});
+sr.reveal('.para-content-2',{delay:100});
+sr.reveal('.para-content-3',{delay:100});
+sr.reveal('.type',{delay:100});
+sr.reveal('.head',{
+    origin:'right',
+    delay:200});
+
+
+sr.reveal('.blog-content',{
+    delay:400});
+
+
+sr.reveal('.foot-head',{
+    origin:'right',
+    delay:200});
+
+
+sr.reveal('.fab',{
+    origin:'left',
+    delay:400});
+
+
+sr.reveal('.copy',{
+    origin:'left',
+    delay:300});
